6dc2065502634157cec43504b4990a2687fbfbc0
borg <guilhermedr@al.insper.edu.br>
2021-03-16 17:54:25 -0300

    Exercícios designados a mim resolvidos

--- Projetos/B-LogicaCombinacional/src/detectorDeMoedas.vhd
+++ Projetos/B-LogicaCombinacional/src/detectorDeMoedas.vhd
@@ -10,5 +10,14 @@ end entity;
 architecture arch of detectorDeMoedas is
 
 begin
+	
+	cents <= "00000" when Q='0' + D='0' + N='0' else
+				"00001" when Q='0' + D='0' + N='1' else
+				"01010" when Q='0' + D='1' + N='0' else
+				"11001" when Q='1' + D='0' + N='0' else
+				"01011" when Q='0' + D='1' + N='1' else
+				"11111" when Q='1' + D='1' + N='0' else
+				"11010" when Q='1' + D='0' + N='1' else
+				"11111" when Q='1' + D='1' + N='1' else;
 
 end architecture;
