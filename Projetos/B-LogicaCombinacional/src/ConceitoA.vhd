5e62ed7cff8255c1491b542935e187f36fcd6008
bernardo Cunha Capoferri <bernardocc@al.insper.edu.br>
2021-03-18 23:04:35 -0300

    Conceitos A e B feitos

--- Projetos/B-LogicaCombinacional/src/ConceitoA.vhd
+++ Projetos/B-LogicaCombinacional/src/ConceitoA.vhd
@@ -29,10 +29,57 @@ architecture rtl of ConceitoA is
 -- signals
 --------------
 
+signal bcdi0 : std_logic_vector(3 downto 0);
+signal bcdi1 : std_logic_vector(3 downto 0);
+signal bcdi2 : std_logic_vector(3 downto 0);
+
+component binarioToBcd is
+generic (N : integer);
+  port (
+        clk, reset: in std_logic;
+        binary_in: in std_logic_vector(N-1 downto 0);
+        bcd0, bcd1, bcd2, bcd3, bcd4: out std_logic_vector(3 downto 0)
+    );
+end component;
+
+component sevenSeg is
+	port (
+			bcd : in  STD_LOGIC_VECTOR(3 downto 0);
+			leds: out STD_LOGIC_VECTOR(6 downto 0));
+end component;
+
 ---------------
 -- implementacao
 ---------------
+
+
 begin
 
+
+seven1: sevenSeg port map(
+		bcd => bcdi0,  -- o sinal a do componente é conectado no sinal i1
+		leds => HEX2  -- o sinal a do componente é conectado no sinal i1
+	);
+	
+seven2: sevenSeg port map(
+		bcd => bcdi1,  -- o sinal a do componente é conectado no sinal i1
+		leds => HEX1  -- o sinal a do componente é conectado no sinal i1
+	);
+	
+seven3: sevenSeg port map(
+		bcd => bcdi2,  -- o sinal a do componente é conectado no sinal i1
+		leds => HEX0  -- o sinal a do componente é conectado no sinal i1
+	);
+
+bintobcd: binarioToBcd generic map(N => 10) port map(
+		clk => CLOCK_50, 
+		reset=> '0',
+		binary_in => SW,
+		bcd0 => bcdi0,
+		bcd1 => bcdi1,
+		bcd2 => bcdi2,
+		bcd3 => open,
+		bcd4 => open
+	);
 
 end rtl;
--- Projetos/B-LogicaCombinacional/src/ConceitoB.vhd
+++ Projetos/B-LogicaCombinacional/src/ConceitoB.vhd
@@ -28,23 +28,14 @@ architecture rtl of ConceitoB is
 --------------
 -- signals
 --------------
-	component sevenSeg is
-		port(
-			bcd : in  STD_LOGIC_VECTOR(3 downto 0);
-			leds: out STD_LOGIC_VECTOR(6 downto 0));
-end component;
 ---------------
 -- implementacao
 ---------------
 begin
-	u1: sevenSeg port map(
-		bcd => SW(3 downto 0),
-		leds => HEX2, 
-	);
-	with SW(7 downto 4) select
-		HEX1 <= "0000001" when "0000", --0
+	with SW(3 downto 0) select
+		HEX2 <= "0000001" when "0000", --0
 				"1001111" when "0001", --1 
-				"0101101" when "0010", --2
+				"0010010" when "0010", --2
 				"0000110" when "0011", --3
 				"1001100" when "0100", --4
 				"0100100" when "0101", --5
@@ -59,10 +50,30 @@ begin
 				"0110000" when "1110", --E
 				"0111000" when "1111", --F
 				"1111111" when others; --Branco caso nao seja um digito
+
+	with SW(7 downto 4) select
+		HEX1 <= "0000001" when "0000", --0
+				"1001111" when "0001", --1 
+				"0010010" when "0010", --2
+				"0000110" when "0011", --3
+				"1001100" when "0100", --4
+				"0100100" when "0101", --5
+				"0100000" when "0110", --6
+				"0001111" when "0111", --7
+				"0000000" when "1000", --8
+				"0000100" when "1001", --9
+				"0000010" when "1010", --A
+				"1100000" when "1011", --B
+				"1110010" when "1100", --C
+				"1000010" when "1101", --D
+				"0110000" when "1110", --E
+				"0111000" when "1111", --F
+				"1111111" when others; --Branco caso nao seja um digito
+
 	with SW(9 downto 8) select
 		HEX0 <= "0000001" when "00", --0
 				"1001111" when "01", --1 
-				"0101101" when "10", --2
+				"0010010" when "10", --2
 				"0000110" when "11", --3
 				"1111111" when others; --Branco caso nao seja um digito
 
