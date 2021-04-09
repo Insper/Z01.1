be732ba423ee84c04f243d6a9b0f02274f8635a3
borg <brunofnr@al.insper.edu.br>
2021-04-09 09:41:28 -0300

    BinaryDigit testado e com imagem

--- Projetos/D-LogicaSequencial/src/BinaryDigit.vhd
+++ Projetos/D-LogicaSequencial/src/BinaryDigit.vhd
@@ -38,4 +38,20 @@ architecture arch of BinaryDigit is
 
 begin
 
+	Mux: Mux2Way port map(
+		a => dffout,
+		b => input,
+		sel => load,
+		q => muxout
+	);
+
+	FF: FlipFlopD port map(
+		clock => clock,
+		d => muxout,
+		clear => '0',
+		preset => '0',
+		q => dffout
+	);
+
+	output <= dffout;
 end architecture;
